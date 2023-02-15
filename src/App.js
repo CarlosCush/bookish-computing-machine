0abf715f7cb618b3a0f6bb59f67070db4527125d
CarlosCush <carloscush@hotmail.com>
2023-02-15 19:11:34 -0300

    Alteracao 2

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Deployment Example DD</p>
+        <p>Deployment Example V2</p>
         <a
           className="App-link"
           href="https://reactjs.org"
